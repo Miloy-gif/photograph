74be494c4f4896740a6124cd6cfe0ab5877c7fed
Mila Tereshchuk <62609425+Miloy-gif@users.noreply.github.com>
2020-07-24 12:16:00 +0300

    main img edit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ let image=document.getElementById('imgs');
 
 
 let img_array=["images/5.jpg", "images/6.jpg", "images/7.jpg", "images/1.jpg", "images/3.jpg", "images/2.jpg",
-  "images/8.jpg", "images/9.jpg",  "images/4.jpg"];
+  "images/8.jpg", "images/9.jpg",  "images/4.jpg", "images/11.jpg",];
 let index=0;
 function slide() {
   document.body.style.backgroundImage = "url("  + img_array[index] + ")";
