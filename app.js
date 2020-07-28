e767c474191cbaa60a5b6c1b51d1ed8d48d2392d
Mila Tereshchuk <62609425+Miloy-gif@users.noreply.github.com>
2020-07-28 13:29:06 +0300

    commit app js images array edit

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 document.getElementById('imgs');
-let img_array=["images/1.jpg","images/2.jpg", "images/7.jpg", "images/9.jpg", "images/3.jpg", "images/5.jpg",
-  "images/6.jpg", "images/12.jpg", "images/13.jpg"];
+
+let img_array=["images/1.jpg","images/2.jpg", "images/9.jpg", "images/3.jpg", "images/5.jpg",
+  "images/6.jpg", "images/12.jpg", "images/13.jpg", "images/7.jpg",];
 let index=0;
 function slide() {
   document.body.style.backgroundImage = "url("  + img_array[index] + ")";
